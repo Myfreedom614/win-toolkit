268eb2fea5024d427abfbafbb4b46d681502207c
Franklin Chen <myfreedom614@gmail.com>
2024-12-05 17:25:12 +0800

    autoSumFromDir.ps1: totalAmount, $totalProblematicAmount

--- autoSumFromDir.ps1
+++ autoSumFromDir.ps1
@@ -4,15 +4,21 @@ param (
 
 $pdfFiles = Get-ChildItem -Path $dirPath -Filter "*.pdf"
 
+$totalAmount = 0
+$totalProblematicAmount = 0
+
 $totalSum = 0
 
 $fileList = @()
 
 foreach ($file in $pdfFiles) {
     $fileName = $file.Name
-    
+
+
     if ($fileName -match "_([0-9\.]+)\.pdf") {
         $number = [double]$matches[1]
+
+        $totalAmount += 1
         
         $totalSum += $number
         
@@ -21,8 +27,15 @@ foreach ($file in $pdfFiles) {
             Number = $number
         }
     }
+    else {
+        $totalProblematicAmount += 1
+    }
 }
 
 $fileList | Format-Table -AutoSize
 
+Write-Output "文件总数：$totalAmount"
+if ($totalProblematicAmount -gt 0) {
+    Write-Output "问题文件总数：$totalProblematicAmount"
+}
 Write-Output "所有数字部分的总和: $totalSum"
